a964456b7a9ecfedf2b61db8a543ca518e861404
Mahad Kalam <mahadkalam1234@gmail.com>
2026-02-05 00:35:47 +0000

    Fix Turnstile

--- config/initializers/turnstiled.rb
+++ config/initializers/turnstiled.rb
@@ -3,4 +3,5 @@
 Rails.application.config.to_prepare do
   Turnstiled.site_key = Rails.app.creds.require(:cloudflare, :turnstile_site_key)
   Turnstiled.site_secret = Rails.app.creds.require(:cloudflare, :turnstile_secret_key)
+  Turnstiled.mock = false  # Use real Cloudflare verification, not mock mode
 end
